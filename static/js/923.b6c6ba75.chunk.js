dab84a0bdf1701f0d0edd238793ce4b921e0848c
PawelYarko <74856939+PawelYarko@users.noreply.github.com>
2022-07-11 18:33:13 +0000

    Deploying to gh-pages from @ PawelYarko/goit-react-hw-05-movies@7a70d99b3f5b4cdd707c03af9f8e9b073ff534b5 🚀

--- static/js/923.b6c6ba75.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[923],{3923:function(e,a,n){n.r(a),n.d(a,{default:function(){return o}});var r=n(501),s="AppBar_navContainer__H8FXw",t="AppBar_nav__eRSYo",c=n(184),o=function(){return(0,c.jsxs)("nav",{className:s,children:[(0,c.jsx)(r.OL,{className:t,to:"/",children:"Home"}),(0,c.jsx)(r.OL,{className:t,to:"/movies",children:"Movies"})]})}}}]);
-//# sourceMappingURL=923.b6c6ba75.chunk.js.map
